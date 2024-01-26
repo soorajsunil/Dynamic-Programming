2c659f3767e4c1b81944f128fc428f09b978381e
Sooraj Sunil <soorajpro@gmail.com>
2024-01-26 17:25:20 -0500

    Add files via upload

---
+++ errorFunction.m
@@ -0,0 +1,20 @@
+function sse = errorFunction(x,y,c,d)
+% sum of squared errors (sse) 
+
+% extract x and y 
+if c>d
+    [c,d]=deal(d,c);
+end
+x = x(c:d);
+y = y(c:d);
+
+% least squares solution
+X     = [ones(size(x)) x];
+theta = X'*X\X'*y;
+
+% Extract coefficients
+a = theta(1);
+b = theta(2);
+yhat = a + b*x;
+sse    = sum((yhat-y).^2);
+end
