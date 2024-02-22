72e5b4ae5df825089fefae5782c207c0313901b1
Sooraj Sunil <soorajpro@gmail.com>
2024-02-22 14:46:25 -0500

    Update errorFunction.m

--- errorFunction.m
+++ errorFunction.m
@@ -8,13 +8,13 @@ end
 x = x(c:d);
 y = y(c:d);
 
-% least squares solution
+% least squares solution 
 X     = [ones(size(x)) x];
-theta = X'*X\X'*y;
+theta = X'*X\X'*y; % not the best implementation but okay for this demo. 
 
 % Extract coefficients
 a = theta(1);
 b = theta(2);
 yhat = a + b*x;
-sse    = sum((yhat-y).^2);
+sse  = sum((yhat-y).^2);
 end
